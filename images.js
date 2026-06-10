adbc32c37648ace08212a2317ac8352864eb847e
1gamer539a <makengoayessa584@gmail.com>
2026-06-10 13:33:23 +0100

    Add files via upload

---
+++ images.js
@@ -0,0 +1,38 @@
+// ── SHOP-STORE | GAME_IMAGES ──
+// Logos officiels pour tous les jeux et abonnements
+const GAME_IMAGES = {
+
+  // ── JEUX ──
+  freefire:      'https://upload.wikimedia.org/wikipedia/fr/4/45/Free_Fire_Logo.png',
+  pubg:          'https://upload.wikimedia.org/wikipedia/fr/6/6a/PUBGlogo.png',
+  ml:            'https://upload.wikimedia.org/wikipedia/fr/thumb/9/98/Mobile_Legends_Bang_Bang_Logo.png/250px-Mobile_Legends_Bang_Bang_Logo.png',
+  fortnite:      'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Fortnite-Logo.svg/512px-Fortnite-Logo.svg.png',
+  roblox:        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Roblox_Logo_2022.svg/512px-Roblox_Logo_2022.svg.png',
+  fc:            'https://upload.wikimedia.org/wikipedia/en/thumb/6/67/EA_Sports_FC_24_cover.png/220px-EA_Sports_FC_24_cover.png',
+  efootball:     'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/EFootball_logo.svg/512px-EFootball_logo.svg.png',
+  bloodstrike:   'https://play-lh.googleusercontent.com/GSwm5F0XWRuoLpVHVRCkAGQZWFLKJrmrUCq_9-3YsPBt3nL2tgHxq0pWJ-sIPrKD9Q=w240-h480',
+  cod:           'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Call_of_Duty_Mobile_Logo.png/320px-Call_of_Duty_Mobile_Logo.png',
+  coc:           'https://upload.wikimedia.org/wikipedia/fr/thumb/f/f2/Clash_of_Clans_Logo.png/240px-Clash_of_Clans_Logo.png',
+  brawlstars:    'https://upload.wikimedia.org/wikipedia/fr/thumb/d/d1/Brawl_Stars_Logo.png/240px-Brawl_Stars_Logo.png',
+  hok:           'https://play-lh.googleusercontent.com/R2yYxGjaFXB5bMw8WmMiLb_Q8ZBqKZWUXu0RnFDzSNSVzEAFJQoiWcGW4BtZf8Z68lk=w240-h480',
+  deltaforce:    'https://play-lh.googleusercontent.com/JC1V2lntGnFuW9b-uEPbLORtJPAJ5y_3gEm-Io1dpRsJtmJlLyYWGUfQ8T9gOBjzKXA=w240-h480',
+  arenabreakout: 'https://play-lh.googleusercontent.com/6g6r_pUZG0QFMlDiU5sGJBLnFAYU-o3zXlSXkmDiJfOxQ0f_w8vKBRsAOy12JFu4DP8=w240-h480',
+  stumbleguys:   'https://upload.wikimedia.org/wikipedia/fr/thumb/d/d0/Stumble_Guys_logo.png/240px-Stumble_Guys_logo.png',
+  genshin:       'https://upload.wikimedia.org/wikipedia/fr/thumb/6/6c/Genshin_Impact_logo.png/320px-Genshin_Impact_logo.png',
+  wutheringwaves:'https://play-lh.googleusercontent.com/Kq1R2n55VUBM28Hxas_cMVEnAUefDHxzMxJLPKoADCbHiNXP7oKi3nFV3kBjN3gxE78=w240-h480',
+  valorant:      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Valorant_logo_-_pink_color_version.svg/512px-Valorant_logo_-_pink_color_version.svg.png',
+  lol:           'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/League_of_Legends_2019_vector.svg/512px-League_of_Legends_2019_vector.svg.png',
+  minecraft:     'https://upload.wikimedia.org/wikipedia/fr/2/2b/Minecraft_logo.png',
+
+  // ── ABONNEMENTS ──
+  netflix:       'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/512px-Netflix_2015_logo.svg.png',
+  snapchat:      'https://upload.wikimedia.org/wikipedia/fr/thumb/c/c4/Snapchat_logo.svg/512px-Snapchat_logo.svg.png',
+  crunchyroll:   'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Crunchyroll_Logo.svg/512px-Crunchyroll_Logo.svg.png',
+  spotify:       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/512px-Spotify_logo_without_text.svg.png',
+  youtube:       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/512px-YouTube_Logo_2017.svg.png',
+  disney:        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Disney%2B_logo.svg/512px-Disney%2B_logo.svg.png',
+  primevideo:    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Amazon_Prime_Video_logo.svg/512px-Amazon_Prime_Video_logo.svg.png',
+  chatgpt:       'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/512px-ChatGPT_logo.svg.png',
+  capcut:        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/CapCut_Logo.svg/512px-CapCut_Logo.svg.png',
+
+};
